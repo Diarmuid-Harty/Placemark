129de69ebd2a758d75c2100bc12c90054fae23e5
Diarmuid-Harty <diarmuidharty1@gmail.com>
2026-02-08 23:51:44 +0100

    Add remaining changes for logging

--- app/src/main/java/ie/setu/placemark/PlacemarkActivity.kt
+++ app/src/main/java/ie/setu/placemark/PlacemarkActivity.kt
@@ -5,12 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
+import timber.log.Timber.i
 
-class MainActivity : AppCompatActivity() {
+class PlacemarkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_placemark)
+
+        Timber.plant(Timber.DebugTree())
+        i("Placemark Activity Started")
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
